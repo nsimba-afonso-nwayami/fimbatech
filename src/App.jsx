a6dbe8a78fadef186cfc7e001b265dd5fc15283d
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2025-11-27 14:15:11 +0000

    App.jsx descartado

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,6 @@ function App() {
 
   return (
     <>
-      <div class="min-h-screen flex items-center justify-center bg-blue-950 text-white p-6 shadow-lg space-x-4">
-        <i class="fas fa-laptop-code text-3xl"></i>
-        <h1 class="text-2xl font-bold">Fimbatech</h1>
-      </div>
     </>
   )
 }
