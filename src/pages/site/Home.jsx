5265463ed49e258218473fd2b8398d9a85dc5653
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2025-11-27 14:16:06 +0000

    Página Home do site

---
+++ src/pages/site/Home.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function Home() {
+  return (
+    <>
+        {/*Header*/}
+        <Header />
+
+        <div class="min-h-screen flex items-center justify-center bg-blue-950 text-white p-6 shadow-lg space-x-4">
+            <i class="fas fa-laptop-code text-3xl"></i>
+            <h1 class="text-2xl font-bold">Fimbatech</h1>
+        </div>
+
+        {/*Footer*/}
+        <Footer />
+    </>
+  );
+}
