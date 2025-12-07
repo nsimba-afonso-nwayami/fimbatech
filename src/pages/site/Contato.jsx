c4a8b2439e2c33a7048490fb1d311d87d6997703
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-12-07 18:39:00 +0100

    Iniciciando página de contato

---
+++ src/pages/site/Contato.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import HeroTechImg from "../../assets/img/hero-tech.jpg";
+
+export default function Tecnologia() {
+  return (
+    <>
+      <title>Contato | FimbaTec</title>
+
+      {/*Header*/}
+      <Header />
+
+      {/*Footer*/}
+      <Footer />
+    </>
+  );
+}
