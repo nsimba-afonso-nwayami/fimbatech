3d2f5bb86b5c7ef90bf18be034fc65403da11690
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-12-07 18:32:48 +0100

    Rota da página tecnologia adicionada

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/site/Home";
 import Sobre from "../pages/site/Sobre";
 import Servicos from "../pages/site/Servicos";
+import Tecnologia from "../pages/site/Tecnologia";
 import NotFound from "../pages/site/NotFound";
 
 export default function AppRoutes () {
@@ -11,6 +12,7 @@ export default function AppRoutes () {
             <Route path="/" element={<Home />} />
             <Route path="/sobre" element={<Sobre />} />
             <Route path="/servicos" element={<Servicos />} />
+            <Route path="/tecnologia" element={<Tecnologia />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     )
