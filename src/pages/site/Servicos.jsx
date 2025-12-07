8f94891720eec17648079011d4efd45a6a16aa1d
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-12-07 11:22:42 +0100

    Página serviços criada

---
+++ src/pages/site/Servicos.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function NotFound() {
+  return (
+    <>
+      <title>Serviços | FimbaTec</title>
+
+      {/*Header*/}
+      <Header />
+
+      {/*Footer*/}
+      <Footer />
+    </>
+  );
+}
