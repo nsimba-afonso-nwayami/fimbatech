f7c7a357b18b919ddabfadd2ccadf2b8d31a8ad1
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-12-07 18:48:19 +0100

    Iniciando

--- src/pages/site/Contato.jsx
+++ src/pages/site/Contato.jsx
@@ -2,9 +2,8 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
-import HeroTechImg from "../../assets/img/hero-tech.jpg";
 
-export default function Tecnologia() {
+export default function Contato() {
   return (
     <>
       <title>Contato | FimbaTec</title>
