e3a8ecb28000922af4b092f262ce04cbde1220f3
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-12-07 12:20:20 +0100

    Atualizaç;ões pontuais

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -123,7 +123,7 @@ export default function Footer() {
               />
               <button
                 type="submit"
-                className="bg-blue-600 px-5 py-2 rounded-lg text-white font-semibold hover:bg-blue-700 transition text-center cursoor-pointer"
+                className="bg-blue-600 text-center cursor-pointer px-5 py-2 rounded-lg text-white font-semibold hover:bg-blue-700 transition cursoor-pointer"
               >
                 Enviar
               </button>
