55e3c332153da2472621aac73350898113dfbf0e
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-12-09 10:43:11 +0100

    Cabeçalho atualizado

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -77,7 +77,7 @@ export default function Header() {
         <Link to="/contato" className="hover:text-cyan-400">Contato</Link>
 
         <Link
-          to="#orcamento"
+          to="/contato"
           className="
             bg-blue-600 px-5 py-2 rounded-lg text-white font-semibold
             hover:bg-blue-700 transition text-center
